a9e015a2a320c348295a7f8dcea12d25dab29448
Akalchi <alejandrafdez1699@gmail.com>
2025-03-31 15:11:15 +0200

    "REFACTOR [BeforeEach] implemented"

--- src/test/java/dev/alejandra/models/AccountTest.java
+++ src/test/java/dev/alejandra/models/AccountTest.java
@@ -1,13 +1,21 @@
 package dev.alejandra.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
- @Test
-     void testDeposit() {
-         Account account = new Account(5000, 5);
-         account.deposit(2000);
-         assertEquals(7000, account.balance);
-     }
+
+    private Account account;
+
+    @BeforeEach
+    void setUp() {
+        account = new Account(5000, 5);
+    }
+
+    @Test
+    void testDeposit() {
+        account.deposit(2000);
+        assertEquals(7000, account.balance);
+    }
 }
