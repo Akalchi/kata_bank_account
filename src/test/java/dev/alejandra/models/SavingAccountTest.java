943de2a6ce16cfd0f49e84d8b2e60776d1ec1135
Akalchi <alejandrafdez1699@gmail.com>
2025-03-31 17:18:48 +0200

    "TEST [CannotWithDrawWhenIsInactive] failed"

--- src/test/java/dev/alejandra/models/SavingAccountTest.java
+++ src/test/java/dev/alejandra/models/SavingAccountTest.java
@@ -1,15 +1,21 @@
 package dev.alejandra.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
 
-      @Test
+    @Test
      void testCannotDepositWhenIsInactive() {
          SavingAccount account = new SavingAccount(5000, 5);
          account.deposit(2000);
+         assertEquals(5000, account.balance);
+     }
+
+    @Test
+    void testCannotWithDrawWhenIsInactive() {
+         SavingAccount account = new SavingAccount(5000, 5);
+         account.withdraw(2000);
          assertEquals(5000, account.balance);
      }
 
