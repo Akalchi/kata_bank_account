c1da93bda83f498eadafce20f603082f4a15342b
Akalchi <alejandrafdez1699@gmail.com>
2025-03-31 17:25:28 +0200

    "TEST [Print] in SavingAccount failed"

--- src/test/java/dev/alejandra/models/SavingAccountTest.java
+++ src/test/java/dev/alejandra/models/SavingAccountTest.java
@@ -1,12 +1,15 @@
 package dev.alejandra.models;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
 
     @Test
-     void testCannotDepositWhenIsInactive() {
+    void testCannotDepositWhenIsInactive() {
          SavingAccount account = new SavingAccount(5000, 5);
          account.deposit(2000);
          assertEquals(5000, account.balance);
@@ -19,8 +22,8 @@ public class SavingAccountTest {
          assertEquals(5000, account.balance);
      }
 
-     @Test
-     void testFeeForExtraWithdrawals() {
+    @Test
+    void testFeeForExtraWithdrawals() {
          SavingAccount account = new SavingAccount(15000, 12);
          account.withdraw(2000);
          account.withdraw(2000);
@@ -31,5 +34,31 @@ public class SavingAccountTest {
          assertEquals(4040, account.balance);
          assertEquals(1000, account.monthlyFee);
      } 
+
+    @Test
+    void testPrint() {
+         SavingAccount account = new SavingAccount(15000, 12);
+         account.withdraw(2000);
+         account.withdraw(2000);
+         account.withdraw(2000);
+         account.withdraw(2000);
+         account.withdraw(2000);
+         account.monthlyStatement();
+         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+         PrintStream printStream = new PrintStream(outputStream);
+         System.setOut(printStream);
+ 
+         account.print();
+ 
+         String expectedOutput = """
+                                 Balance: 4040.0
+                                 Monthly Fee: 1000.0
+                                 Total transactions: 5
+                                 """;
+ 
+         assertEquals(expectedOutput, outputStream.toString());
+ 
+         System.setOut(System.out);
+     }
 
 }
